e557db2100d86327238132b0837a4ac72baed6cf
Anurag Kumar <115798514+anurag21213@users.noreply.github.com>
2024-09-22 12:51:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faBars} from "@fortawesome/free-solid-svg-icons/faBars"
 import MobileNav from './MobileNav'
-import NavContext from '../contextAPI/NavContext/navContext'
+import NavContext from '../contextAPI/NavContext/NavContext'
 
 
 const Navbar = () => {
